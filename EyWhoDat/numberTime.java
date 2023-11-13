9a50d36b75fcf0009a349fb2825da76a64276779
J.F.Swalwell <148398869+PR0T0C0L-ZER0@users.noreply.github.com>
2023-11-13 15:14:33 +0000

    number pyramid

---
+++ EyWhoDat/numberTime.java
@@ -0,0 +1,14 @@
+class numberTime
+{
+    public static void main(String args[])
+    {
+        System.out.println("Hiya!");
+        System.out.println("");
+        System.out.println("   1   ");
+        System.out.println("  212  ");
+        System.out.println(" 32123 ");
+        System.out.println("4321234");
+        System.out.println("");
+        System.out.println("Thank you for your time!");
+    }
+}
