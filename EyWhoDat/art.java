7a913a3a62c2de22942cf21f7f0ea42933bc2261
J.F.Swalwell <148398869+PR0T0C0L-ZER0@users.noreply.github.com>
2023-11-13 16:07:00 +0000

    kitty art

---
+++ EyWhoDat/art.java
@@ -0,0 +1,24 @@
+class art
+{
+    public static void main(String args[])
+    {
+        System.out.println("")
+        System.out.println("He Sees You");
+        System.out.println("");
+        System.out.println("      _.---.._             _.---...__");
+        System.out.println("   .-'   /\\   \\          .'  /\\     /");
+        System.out.println("   `.   (  )   \\        /   (  )   /");
+        System.out.println("     `.  \\/   .'\\      /`.   \\/  .'");
+        System.out.println("       ``---''   )    (   ``---''");
+        System.out.println("               .';.--.;`.");
+        System.out.println("             .' /_...._\\ `.");
+        System.out.println("           .'   `.a  a.'   `.");
+        System.out.println("          (        \\/        )");
+        System.out.println("           `.___..-'`-..___.'");
+        System.out.println("              \\          /");
+        System.out.println("               `-.____.-'");
+        System.out.println("");
+        System.out.println("Thank you for viewing");
+        System.out.println("");
+    }
+}
