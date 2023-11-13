163f2c8f79dae4ec6c95f0714ae95d882bf8e632
J.F.Swalwell <148398869+PR0T0C0L-ZER0@users.noreply.github.com>
2023-11-13 15:08:10 +0000

    kitty cat, ritty rat

---
+++ EyWhoDat/storyTime.java
@@ -0,0 +1,28 @@
+class storyTime
+{
+    public static void main(String args[])
+    {
+        System.out.println();
+        System.out.println("One day, there was a cat.");
+        System.out.println();
+        System.out.println("One day, the cat met a rat.");
+        System.out.println();
+        System.out.println("The rat was scared the cat would eat it.");
+        System.out.println();
+        System.out.println("But the cat did not!");
+        System.out.println();
+        System.out.println("The cat befriended the rat");
+        System.out.println();
+        System.out.println("The cat and the rat would play every day,");
+        System.out.println();
+        System.out.println("But one day, the rat could not find the cat");
+        System.out.println();
+        System.out.println("The rat eventually found the cat,");
+        System.out.println();
+        System.out.println("But the cat didn't say hello, or anything at all");
+        System.out.println();
+        System.out.println("The cat was there, but he was gone.");
+        System.out.println();
+        System.out.println("So the rat left sad, knowing the cat was lost.");
+    }
+}
