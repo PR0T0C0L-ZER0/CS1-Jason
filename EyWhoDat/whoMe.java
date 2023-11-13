8ba000fad6428729b57f3943e9040e769ef4ee07
J.F.Swalwell <148398869+PR0T0C0L-ZER0@users.noreply.github.com>
2023-11-13 14:42:37 +0000

    Buy gold!

---
+++ EyWhoDat/whoMe.java
@@ -0,0 +1,25 @@
+class whoMe
+{
+    public static void main(String args[])
+    {
+        {
+            System.out.println();
+
+            System.out.println("Hiya! Nice to meet you!");
+
+            System.out.println();
+            
+            System.out.println("My name is Jason.");
+
+            System.out.println();
+
+            System.out.println("I am seventeen years of age");
+
+            System.out.println();
+
+            System.out.println("And remember! Reality's an illusion, the universe is a hologram, buy gold! Byeeee!");
+
+            System.out.println();
+        }
+    }
+}
