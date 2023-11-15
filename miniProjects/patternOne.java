7f500a431bbeaaa928f994384a470a268d8a324e
J.F.Swalwell <148398869+PR0T0C0L-ZER0@users.noreply.github.com>
2023-11-15 14:28:28 +0000

    e

--- miniProjects/patternOne.java
+++ miniProjects/patternOne.java
@@ -2,6 +2,12 @@ class patternOne
 {
     public static void main(String args[])
     {
-
+        System.out.println();
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("******");
+        System.out.println();
     }
 }
