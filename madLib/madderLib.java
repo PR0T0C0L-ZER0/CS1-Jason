fe3cc8a47c58cd37ba9703d0cb80b8923f1f3d9a
J.F.Swalwell <148398869+PR0T0C0L-ZER0@users.noreply.github.com>
2023-11-27 15:20:46 +0000

    e

---
+++ madLib/madderLib.java
@@ -0,0 +1,20 @@
+class madderLib
+{
+    public static void main(String args[])
+    {
+        {
+            String ="";
+            String ="";
+            String ="";
+            String ="";
+            String ="";
+            String ="";
+            String ="";
+            String ="";
+            String ="";
+            String ="";
+        }
+
+        System.out.print("")
+    }
+}
