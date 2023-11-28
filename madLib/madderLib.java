68ee055ded3adbac589c6bed378991b3304fc78b
J.F.Swalwell <148398869+PR0T0C0L-ZER0@users.noreply.github.com>
2023-11-28 02:56:31 +0000

    e

--- madLib/madderLib.java
+++ madLib/madderLib.java
@@ -2,19 +2,21 @@ class madderLib
 {
     public static void main(String args[])
     {
-        {
-            String ="";
-            String ="";
-            String ="";
-            String ="";
-            String ="";
-            String ="";
-            String ="";
-            String ="";
-            String ="";
-            String ="";
-        }
-
-        System.out.print("")
+        
+            String adjOne = "word";
+            String foodsPluralOne = "word";
+            String verbOne = "word";
+            String personSays = "word";
+            String nounOne = "word";
+            String foodsPluralTwo = "qord";
+            String colorOne = "word";
+            String aRideOne = "word";
+            String animalOne = "word";
+            String personsName = "word";
+        
+        System.out.println("");
+        System.out.print("Today I went to my favorite Taco Stand called the "+ adjOne + " " + animalOne + ". Unlike most food stands, they cook and prepare the food in a " + aRideOne + " while you " + verbOne + ". The best thing on the menu is the " + colorOne + " " + nounOne + ". Instead of ground beef they fill the taco with " + foodsPluralTwo + ", cheese, and top it off with a salsa made from " + foodsPluralOne + ". If that doesn't make your mouth water, then it's just like " + personsName + " always says: " + personSays + "!");
+        System.out.println("");
+        System.out.println("");
     }
 }
