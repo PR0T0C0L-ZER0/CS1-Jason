f931468cd01ce50205fdfefcddea8c3ffc298358
J.F.Swalwell <148398869+PR0T0C0L-ZER0@users.noreply.github.com>
2023-12-11 14:59:49 +0000

    e

---
+++ arithmatic/prob1.java
@@ -0,0 +1,13 @@
+class prob1
+{
+    public static void main(String args[])
+    {
+        int num1=1;
+        int num2=2;
+
+        int sum;
+        sum=num1+num2;
+
+        System.out.println(sum);
+    }
+}
