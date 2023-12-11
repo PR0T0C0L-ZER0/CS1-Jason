886686921a5e91d504c0d38dcb84466016ffd466
J.F.Swalwell <148398869+PR0T0C0L-ZER0@users.noreply.github.com>
2023-12-11 15:17:10 +0000

    e

---
+++ miniMathProblems/prob3.java
@@ -0,0 +1,21 @@
+class prob3
+{
+    public static void main(String args[])
+    {   //for all lines, erase the zeros and type actual lengths
+
+        //side one
+            int side1=000;
+        //side two
+            int side2=000;
+        //side three
+            int side3=000;
+        //side four
+            int side4=000;
+
+
+
+        int perimeter=(side1+side2+side3+side4);
+
+        System.out.println("the perimeter of the rectangle is " + perimeter);
+    }
+}
