2690ffe52697f5996ecb73ba07cd81d68f35b800
J.F.Swalwell <148398869+PR0T0C0L-ZER0@users.noreply.github.com>
2023-12-13 14:53:51 +0000

    e

---
+++ miniMathProblems/prob6.java
@@ -0,0 +1,15 @@
+class prob6
+{
+    public static void main(String args[])
+        {//numbers to be averaged together
+
+            int num1=0;
+            int num2=0;
+            int num3=0;
+
+            
+            //average
+            
+            System.out.println((num1+num2+num3)/3);
+        }
+}
