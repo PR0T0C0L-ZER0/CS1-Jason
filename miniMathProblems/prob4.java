ecdb04e91ccf9d1cc650218e55aa85cef7f36bb8
J.F.Swalwell <148398869+PR0T0C0L-ZER0@users.noreply.github.com>
2023-12-13 13:51:29 +0000

    e

---
+++ miniMathProblems/prob4.java
@@ -0,0 +1,21 @@
+class prob4
+{
+    public static void main(String args[])
+    {   //input length of sides where zeros are
+
+        
+        //Top side 
+            int topSide=000;
+        //Bpttom side
+            int bottomSide=000;
+        //slanted side, assuming both are of equal length and angle
+            int slantSide=000;
+
+
+        //perimeter
+            int peri=(topSide+bottomSide+(slantSide*2));
+
+            System.out.println(peri);
+
+    }
+}
