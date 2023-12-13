b7450cfb5948fef90ee31450f687b5552308be3b
J.F.Swalwell <148398869+PR0T0C0L-ZER0@users.noreply.github.com>
2023-12-13 15:04:18 +0000

    e

---
+++ miniMathProblems/prob9.java
@@ -0,0 +1,13 @@
+class prob9
+
+{
+    public static void main(String args[])
+    {
+        Float fahrenheit=32f;
+
+
+        Float celcius=(fahrenheit - 32) * (5/9);
+
+        System.out.println(fahrenheit + " degrees in fahrenheit is " + celcius + " degrees in celcius");
+    }
+}
