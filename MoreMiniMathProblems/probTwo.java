391917af4644da581a06f167a183aa87fbd2dae6
J.F.Swalwell <148398869+PR0T0C0L-ZER0@users.noreply.github.com>
2024-01-08 16:21:33 +0000

    e

--- MoreMiniMathProblems/probTwo.java
+++ MoreMiniMathProblems/probTwo.java
@@ -3,7 +3,7 @@ class probTwo
 {
     public static void main(String args[])
     {
-        int num=5;
+        int num=4;
 
 
         System.out.println(num+" cubed then halved is "+((num*num*num)/2));
