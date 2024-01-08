1d4d24f6d04ffaac3dc270d6e9c8950c975add5e
J.F.Swalwell <148398869+PR0T0C0L-ZER0@users.noreply.github.com>
2024-01-08 16:15:08 +0000

    e

---
+++ MoreMiniMathProblems/probOne.java
@@ -0,0 +1,16 @@
+class probOne
+{
+    public static void main(String args[])
+        {//numbers to be averaged together
+
+            int num1=0;
+            int num2=0;
+            int num3=0;
+            int num4=0;
+
+            
+            //average
+            
+            System.out.println((num1+num2+num3+num4)/4);
+        }
+}
---
+++ MoreMiniMathProblems/probTwo.java
@@ -0,0 +1,11 @@
+class probTwo
+
+{
+    public static void main(String args[])
+    {
+        int num=5;
+
+
+        System.out.println(num+" cubed then halved is "+((num*num*num)/2));
+    }
+}
