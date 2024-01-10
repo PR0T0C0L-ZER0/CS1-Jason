cc36415a53e5d8d9039963a6597ce5c1aea112a7
J.F.Swalwell <148398869+PR0T0C0L-ZER0@users.noreply.github.com>
2024-01-10 16:06:41 +0000

    e

--- MoreMiniMathProblems/probThree.java
+++ MoreMiniMathProblems/probThree.java
@@ -1,8 +1,22 @@
-class probThree
+import java.lang.Math;
 
+class probThree 
 {
     public static void main(String args[])
     {
-        
+        //base values
+        double radius=5;
+        double height=8;
+
+        //volume
+        double circsurf=((Math.PI)*(radius*radius));
+        double volume=(circsurf*height);
+
+        //surface area
+        double circumfrence=((Math.PI)*(radius*2));
+        double edgeArea=(circumfrence*height);
+        double totalArea=((circsurf*2)+edgeArea);
+
+        System.out.print("for a cylinder with a height of "+height+" inches and a radius of "+radius+" inches, the total surface area is "+totalArea+" square inches, and the volume is a total of "+volume+" cubic inches.");
     }
 }
